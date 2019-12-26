f98eaa91994beb2dd3884a29ddfd095ac12ffaef
Mikhail Zykov <mikhail.zykov@corp.vk.com>
2019-12-26 17:30:23 +0300

    Template was added

---
+++ precache-manifest.b4449988f52c2bbd4e63b666d77f11d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34426a265cd40e880134dc128d3756dd",
+    "url": "/Schedule/index.html"
+  },
+  {
+    "revision": "07fb1911486c8dc7aaf6",
+    "url": "/Schedule/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "35fe5f2986aca7c076dc",
+    "url": "/Schedule/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "07fb1911486c8dc7aaf6",
+    "url": "/Schedule/static/js/2.f6d81551.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Schedule/static/js/2.f6d81551.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35fe5f2986aca7c076dc",
+    "url": "/Schedule/static/js/main.3aee6dbf.chunk.js"
+  },
+  {
+    "revision": "ccbf792e3520630db1f1",
+    "url": "/Schedule/static/js/runtime-main.6a90bc29.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/Schedule/static/media/persik.4e1ec840.png"
+  }
+]);
